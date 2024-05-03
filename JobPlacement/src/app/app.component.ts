d8a9f114bdf31f61bec9131ecd95cfe98bb16b71
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-05-03 02:13:19 +0200

    Import provider for httpClient

--- JobPlacement/src/app/app.component.ts
+++ JobPlacement/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule ,RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
